c13be1819b40adeef702ee19883109b0ab35c923
Matias98cs <matias98cs@gmail.com>
2022-11-23 20:39:49 -0300

    cors

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import cors from 'cors'
 
 const app = express()
 dotenv.config()
 connectDB()
 
-app.use("/", (res, req) => {
-    res.send('Hola')
-})
+const dominiosPermitidos = [process.env.FRONTEND_URL]
+const corsOptiones = {
+    origin: function(origin, callback) {
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            //el origuen del request esta permitido
+            callback(null, true)
+        }else {
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptiones))
+// app.use('/api/user', )
 
 const PORT = process.env.PORT || 4000
 
