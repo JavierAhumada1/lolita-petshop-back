c6e58e444b474c958ee9694cb580a461e58695a3
Matias98cs <matias98cs@gmail.com>
2022-11-24 09:42:24 -0300

    method authenticateUser and rutes post

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import generateJWT from '../helpers/generateJWT.js'
 import User from '../models/userModels.js'
 
 const register = async (req, res) => {
@@ -12,7 +13,7 @@ const register = async (req, res) => {
         const userSaved = await user.save()
 
         res.status(200).json({
-            msg: 'Registrando usaurio',
+            msg: 'Registrando usuario',
             // para mostrar los datos que se crearon pero no se debe mostrar en el lado del cliente
             // userSaved
         })
@@ -43,7 +44,39 @@ const confirmUser = async (req, res) => {
     }
 }
 
+const authenticateUser = async (req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    if(!user){
+        const error = new Error('El usuario no existe')
+        return res.status(400).json({
+            msg: error.message
+        })
+    }
+    if(!user.confirm){
+        const error = new Error('Tu cuenta no ha sido confirmada')
+        return res.status(400).json({
+            msg: error.message
+        })
+    }
+    if(await user.checkPassword(password)){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateJWT(user.id)
+        })
+    }else {
+        const error = new Error('El password es incorrecto')
+        return res.status(400).json({
+            msg: error.message
+        })
+    }
+}
+
 export {
     register,
-    confirmUser
+    confirmUser,
+    authenticateUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { register, confirmUser } from '../controllers/userController.js'
+import { register, confirmUser, authenticateUser } from '../controllers/userController.js'
 
 const router = express.Router()
 router.post('/', register)
 router.get('/confirm/:token', confirmUser)
+router.post('/login', authenticateUser)
 
 export default router
