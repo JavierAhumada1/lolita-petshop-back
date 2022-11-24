194a08e503935bea531003a59e213a40cf2c13e7
Matias98cs <matias98cs@gmail.com>
2022-11-23 22:52:49 -0300

    route get for confirm with token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { register } from '../controllers/userController.js'
+import { register, confirmUser } from '../controllers/userController.js'
 
 const router = express.Router()
 router.post('/', register)
+router.get('/confirm/:token', confirmUser)
 
 export default router
