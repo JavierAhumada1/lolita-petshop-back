2a0f5433f6a35eb662e6c51ac7f3475538585721
Matias98cs <matias98cs@gmail.com>
2022-11-24 23:36:31 -0300

    delete and update accessories

--- controllers/accessoriesController.js
+++ controllers/accessoriesController.js
@@ -26,8 +26,47 @@ const readAll = async(req, res) => {
     res.status(200).json({accessories})
 }
 
+const deleteProduct = async(req, res) => {
+    const {id} = req.params
+    const accessorie = await Accessories.findById(id)
+    if(!accessorie){
+        const error = new Error('Hubo un error')
+        return res.status(400).json({msg: error.message})
+    }
+    try {
+        await accessorie.deleteOne()
+        res.status(200).json({msg: 'Producto eliminado'})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const updateAccessories = async(req, res) => {
+    const {id} = req.params
+    const accessorie = await Accessories.findById(id)
+    if(!accessorie){
+        const error = new Error('Producto no encontrado')
+        return res.status(400).json({msg: error.message})
+    }
+
+    accessorie.type = req.body.type || accessorie.type
+    accessorie.name = req.body.name || accessorie.name
+    accessorie.measures = req.body.measures || accessorie.measures
+    accessorie.price = req.body.price || accessorie.price
+    accessorie.code = req.body.code || accessorie.code
+    accessorie.description = req.body.description || accessorie.description
+    try {
+        const accessorieUpdate = await accessorie.save()
+        res.status(200).json({accessorieUpdate})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export {
     createAccessorie,
     readProductById,
-    readAll
+    readAll,
+    deleteProduct,
+    updateAccessories
 }
--- routes/accessorieRoutes.js
+++ routes/accessorieRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { createAccessorie, readProductById, readAll } from '../controllers/accessoriesController.js'
+import { createAccessorie, readProductById, readAll, deleteProduct, updateAccessories } from '../controllers/accessoriesController.js'
 
 const router = express.Router()
 
+//area publica
 router.post('/', createAccessorie)
 router.get('/:id', readProductById)
 router.get('/', readAll)
+
+//area privada
+router.delete('/:id', deleteProduct)
+router.patch('/:id', updateAccessories)
 
 export default router
